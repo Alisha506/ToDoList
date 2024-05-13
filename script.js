78a755499477fd37a2ea7c1f5351cc03f9929ab1
Alisha Ali <137915040+Alisha506@users.noreply.github.com>
2024-05-13 01:14:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `<div class="task"> <span>${
+      document.querySelector("#newtask input").value
+    }</span> <button class="delete"> <i class="fa-regular fa-trash-can"></i> </button> </div>`;
+
+    var current_task = document.querySelectorAll(".delete");
+
+    for (var i = 0; i < current_task.length; i++) {
+      current_task[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+
+    var tasks = document.querySelectorAll(".task");
+    for (var i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+
+    document.querySelector("#newtask input").value = "";
+  }
+};
